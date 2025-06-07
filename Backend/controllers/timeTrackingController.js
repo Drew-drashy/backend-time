db9c079634e6bf814b53d59d645c6944cb14a149
Drew-drashy <107747111+Drew-drashy@users.noreply.github.com>
2025-06-07 21:10:46 +0530

    done

--- Backend/controllers/timeTrackingController.js
+++ Backend/controllers/timeTrackingController.js
@@ -1,4 +1,4 @@
-const TimeLog = require('../models/TimeLog');
+const TimeLog = require('../models/Timelog');
 const Project =require('../models/Project');
 const { checkIfInsideGeofence } = require('../utils/geoUtils');
 
